deb7224213675ec73402c3e0c2e77ae4132947ec
gh-charles <blakos244@gmail.com>
2023-08-02 15:52:17 -0500

    add new_ext

--- convert program.py
+++ convert program.py
@@ -1,5 +1,7 @@
 import moviepy.editor as moviepy
 
-def convert_video(file_name: str):
+def convert_video(file_name: str, new_ext: str):
     split_file_name = file_name.rsplit('.')
     clip = moviepy.VideoFileClip(file_name)
+    clip.write_videofile(split_file_name[0] + new_ext)
+    
