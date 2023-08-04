e3ef0df534520a681ac38ffa07b60017c7538880
gh-charles <blakos244@gmail.com>
2023-08-04 05:23:10 -0500

    add main loop

--- convert program.py
+++ convert program.py
@@ -6,17 +6,20 @@ def convert_video(file_name: str, new_ext: str):
     clip.write_videofile(split_file_name[0] + new_ext)
 
 def main():
-    choice = input('Do you want to convert images, or video? Enter in i and v respectively: ')
-    if choice.lower() == 'i':
-        image_path = input('Enter the path of the image to convert: ')
-        new_ext = input('Enter the new type of the image will be, like this: .exe ')
-        convert_image(image_path, new_ext)
+    while True:
+        choice = input('Do you want to convert images, or video? Enter in i and v respectively: ')
+        if choice.lower() == 'i':
+            image_path = input('Enter the path of the image to convert: ')
+            new_ext = input('Enter the new type of the image will be, like this: .exe ')
+            convert_image(image_path, new_ext)
 
-    elif choice.lower() == 'v':
-        video_path = input('Enter the path of the video to convert: ')
-        new_ext = input('Enter the new type of the video will be, like this: .exe ')
-        convert_video(video_path, new_ext)
+        elif choice.lower() == 'v':
+            video_path = input('Enter the path of the video to convert: ')
+            new_ext = input('Enter the new type of the video will be, like this: .exe ')
+            convert_video(video_path, new_ext)
 
-    else:
-        print('Try again.')
+        else:
+            print('Try again.')
+
+main()
 
