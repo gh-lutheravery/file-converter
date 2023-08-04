adaeecd1b9addaf6284cd34761761a09afad5cf5
gh-charles <blakos244@gmail.com>
2023-08-04 04:41:30 -0500

    convert video in elif

--- convert program.py
+++ convert program.py
@@ -6,8 +6,12 @@ def convert_video(file_name: str, new_ext: str):
     clip.write_videofile(split_file_name[0] + new_ext)
 
 def main():
-    choice = input('Do you want to convert images, or video? Enter in i and v respectively.')
+    choice = input('Do you want to convert images, or video? Enter in i and v respectively: ')
     if choice.lower() == 'i':
-    
-    else if choice.lower() == 'v':
+        print()
+    elif choice.lower() == 'v':
+        video_path = input('Enter the path of the video to convert: ')
+        new_ext = input('Enter the new type of the video will be, like this: .exe ')
+        convert_video(video_path, new_ext)
+
         
