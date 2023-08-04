5c87d13f9d2c6a8a46f413c59ab5d808b915fb5e
gh-charles <blakos244@gmail.com>
2023-08-04 04:06:44 -0500

    input logic for main

--- convert program.py
+++ convert program.py
@@ -4,4 +4,10 @@ def convert_video(file_name: str, new_ext: str):
     split_file_name = file_name.rsplit('.')
     clip = moviepy.VideoFileClip(file_name)
     clip.write_videofile(split_file_name[0] + new_ext)
+
+def main():
+    choice = input('Do you want to convert images, or video? Enter in i and v respectively.')
+    if choice.lower() == 'i':
     
+    else if choice.lower() == 'v':
+        
