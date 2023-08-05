85d503e4599df6777c4d985d1373a598c4fe6fae
gh-charles <blakos244@gmail.com>
2023-08-05 17:15:17 -0500

    apply str_aspect_ratio

--- convert program.py
+++ convert program.py
@@ -21,7 +21,7 @@ def convert_video(file_name: str, new_ext: str):
     split_file_name = file_name.rsplit('.')
     try:
         clip = moviepy.VideoFileClip(file_name)
-        clip.write_videofile(split_file_name[0] + new_ext)
+        clip.write_videofile(split_file_name[0] + new_ext, ffmpeg_params=['-aspect', str_aspect_ratio])
     except Exception as e:
         print('Error occurred during conversion: ' + e)
 
